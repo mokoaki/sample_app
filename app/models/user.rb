6382cf1c71bb3c42211c62f037515c843dececf3
mokoaki <mokoriso@gmail.com>
2014-03-09 18:22:34 +0900

    6 end 結局、毎回テスト用に作ってるメアドをランダムに作ったら動いた　他の連中はどうしてるんだ？？

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence:   true,
-										format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-										uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6 }
+	before_save { self.email.downcase! }
 	has_secure_password
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence:   true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
 end
