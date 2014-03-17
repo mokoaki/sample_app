9e1229dc9fbd61e9aa0d333459de7fa5c849f149
mokoaki <mokoriso@gmail.com>
2014-03-18 02:18:10 +0900

    8 end あ、まだ演習やってねえ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,24 @@
 class User < ActiveRecord::Base
 	before_save { self.email.downcase! }
+	before_create :create_remember_token
+
 	has_secure_password
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence:   true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+	def create_remember_token
+		self.remember_token = User.encrypt(User.new_remember_token)
+	end
 end
