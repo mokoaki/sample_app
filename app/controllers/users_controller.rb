e1120d3cb7bf8cc1fbf0833d2bbaaad913c1a160
mokoaki <mokoriso@gmail.com>
2014-03-18 23:33:58 +0900

    お勉強の為に各行にコメント入れ中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,29 @@
 class UsersController < ApplicationController
-
 	def show
+		#マイページ params[:id]を元にDBから情報取得、画面へ渡す
 		@user = User.find(params[:id])
 	end
 
 	def new
+		#登録画面にて使う。値はカラッポ
 		@user = User.new
 	end
 
 	def create
+		#登録画面で入力した情報を元にユーザ作成する user_paramsが噛んでる
 		@user = User.new(user_params)
 
 		if @user.save
+			#ログイン処理
 			sign_in @user
+
+			#ユーザ登録出来たよメッセージ ちなみに flash.nowじゃないのはこの後リダイレクトするから
 			flash[:success] = "Welcome to the Sample App!"
+
+			#マイページ遷移
 			redirect_to @user
 		else
+			#ユーザ作成失敗、登録画面に戻り、エラーメッセージを表示する
 			render :new
 		end
 	end
@@ -23,6 +31,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
+		#StrongParameters対応。user必須、その配下の許可リスト、みたいな
 		params.require(:user).permit(:name ,:email, :password, :password_confirmation)
 	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,35 @@
 module SessionsHelper
+	#このヘルパはapplication_controllerでincludeされてるので全ての箇所で使える（多分
+	#ならapplication_helperに書けばいんじゃねとか思うけど後で考える
+
 	def sign_in(user)
+		#ユーザ作成時、ログイン時に呼ばれる
+
+		#毎回ログイントークン変える
 		remember_token = User.new_remember_token
+
+		#ログイントークンはクッキーで持つ。画面遷移毎にこいつを使ってユーザ確認する
 		cookies.permanent[:remember_token] = remember_token
+
+		#暗号化したログイントークンはDBに持つ
 		user.update_attribute(:remember_token, User.encrypt(remember_token))
+
+		#@current_userへユーザ格納
 		self.current_user = user
 	end
 
 	def signed_in?
+		#current_userが有効かどうかでログイン済かどうか判断
 		!current_user.nil?
 	end
 
 	def current_user=(user)
+		#アクセサ これって@current_userカプセル化出来てんの？　出来てないか
 		@current_user = user
 	end
 
 	def current_user
+		#2度目以降のアクセスはもちろん値を返すだけ　逆に言うと画面遷移時には最低1回はDBアクセスが発生する
 		@current_user ||= User.find_by(remember_token: User.encrypt(cookies[:remember_token]))
 	end
 
