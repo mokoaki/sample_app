231e7e5e1c4ca37b0ef724c16cd09b596b0d5b23
mokoaki <mokoriso@gmail.com>
2014-03-28 23:53:23 +0900

    10 end 2

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,14 +1,19 @@
 module MicropostsHelper
   def wrap(content)
-    #改行対応してないの気持ち悪いな
-    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+    sanitize(raw(wrap_long_string_and_gsub_newline_to_br_tag(content)))
   end
 
   private
 
-  def wrap_long_string(text, max_width = 30)
-    zero_width_space = "&#8203;"
-    regex = /.{1,#{max_width}}/
-    (text.length < max_width) ? text : text.scan(regex).join(zero_width_space)
+  def wrap_long_string_and_gsub_newline_to_br_tag(content)
+    max_width        = 30
+    zero_width_space = '&#8203;'
+    regex            = /.{1,#{max_width}}/
+
+    content.split(/\r\n|\r|\n/).map do |content_line|
+      content_line.split(' ').map do |text|
+        (text.length < max_width) ? text : text.scan(regex).join(zero_width_space)
+      end.join(' ')
+    end.join('<br />')
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
-  describe "GET 'new'" do
+  describe "GET new" do
     it "returns http success" do
       get 'new'
       response.should be_success
